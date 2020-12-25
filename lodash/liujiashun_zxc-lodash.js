0f0934f6b1a90f3b05307c9b289a58ab374c1806
ljs <71818679+liujiashun-zxc@users.noreply.github.com>
2020-12-25 22:13:40 +0800

    Update liujiashun_zxc-lodash.js

--- lodash/liujiashun_zxc-lodash.js
+++ lodash/liujiashun_zxc-lodash.js
@@ -1,15 +1,22 @@
-    function chunk(array, size) { 
-      size = Math.max(size, 0) 
-      const length = array == null ? 0 : array.length 
-      if (!length || size < 1) {
-         return [] 
-        } 
-      let index = 0 
-      let resIndex = 0 
-      const result = new Array(Math.ceil(length / size))
-       while (index < length) {
-          result[resIndex++] = slice(array, index, (index += size))
-         } 
-         return result 
-        },
+function chunk(ary, size = 1) {
+    var result = []
+    var arr = []
+    var l = 0
+    if (size < 1) {
+        size = 1
+    }
+    for (i = 0; i < ary.length; i++) {
+        arr.push(ary[i])
+        l++
+        if (l == size) {
+            result.push(arr)
+            arr = []
+            l = 0
+        }
+    }
+    if (l != 0) {
+        result.push(arr)
+    }
+    return result
+}
           
