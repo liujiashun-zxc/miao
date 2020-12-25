2e164739728ee9afca2ad11ea33fec9ecf135a04
ljs <71818679+liujiashun-zxc@users.noreply.github.com>
2020-12-25 21:57:47 +0800

    Update liujiashun_zxc-lodash.js

--- lodash/liujiashun_zxc-lodash.js
+++ lodash/liujiashun_zxc-lodash.js
@@ -4,51 +4,17 @@ var liujiashun_zxc = {
   },
   compact: function(array) {
     
-  }
-        function concat(arr,arg){
-        let result = arr
-        for(let item of arg){
-          if(Array.isArray(item)){
-            result.push(subitem)
-          }
-          else{
-            retult.push(item)
-          }
-        }
-        return retult
+  },
+  concat : function (arr,arg){
+  let result = arr
+    for (let item of arg) {
+      if (Array.isArray(item)) {
+        result.push(subitem)
       }
-      function difference(array,values){
-        let iteratee = last(values)
-        if(isArrayLikeObject(iteratee)){
-          iteratee = undefined
-        }
-        return isArrayLikeObject(array)
-        ? baseDifferemce(array,baseFlatten(values,1,isArrayLikeObject,true),iteratee)
-        :[]
+      else {
+        retult.push(item)
       }
-     let_ = require("lodash"){
-     let result = _.differenceBy([12, 3, 4], [3, 1], function (item) {
-       return item - 3 > 0
-     })
-     console.log(result)
-   }
-function:differenceWith(comperator，values){
-  for(let i = 0; i < values.length; i++){
-     if(comparator(comuted,values[i])){
-       return true
-     }
-     return false
-   }
-}
-function drop(){
-  
-}
-
-
-
-
-
-
-
+      return retult
+    }
 
 }
