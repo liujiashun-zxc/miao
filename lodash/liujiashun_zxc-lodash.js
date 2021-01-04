e06a5b7f8558de05ad7831d2fe5ce04ae2acf546
ljs <71818679+liujiashun-zxc@users.noreply.github.com>
2021-01-04 21:43:23 +0800

    Update liujiashun_zxc-lodash.js

--- lodash/liujiashun_zxc-lodash.js
+++ lodash/liujiashun_zxc-lodash.js
@@ -1,30 +1,28 @@
-  var liujiashun = function chunk(ary,size = 1){
-   var result = []
-   var arr = []
-   var l = 0
-   if (size < 1) {
-    size = 1
-   }
-  for (i = 0; i < ary.length; i++) {
-    arr.push(ary[i])
-    l++
-    if (l == size) {
-      result.push(arr)
-      arr = []
-      l = 0
-    }
-  }
-  if (l != 0) {
-    result.push(arr)
-  }
-  return result
-  }
-function compact(ary) {
-    var result = []
-    for (i = 0; i < ary.length; i++) {
-        if (ary[i]) {
-            result.push(ary[i])
+    var liujiashun_zxc = function() {
+      function chunk(ary, size = 1) {
+      var result = []
+      var arr = []
+      var l = 0
+      if (size < 1) {
+        size = 1
+      }
+      for (i = 0; i < ary.length; i++) {
+        arr.push(ary[i])
+        l++
+
+        if (l == size) {
+          result.push(arr)
+          arr = []
+          l = 0
         }
+
+      }
+
+      if (l != 0) {
+        result.push(arr)
+      }
+
+      return result
     }
-    return result
-}
+  }
+
